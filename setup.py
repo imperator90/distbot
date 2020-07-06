d710c7fff42b98e94ab5f409b9827d5f1862805b
Dan <danielkelleher@protonmail.com>
2020-07-06 09:25:04 -0400

    change release number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 setuptools.setup(
     name='pyppeteer_spider',
-    version='1.0.3',
+    version='1.1.0',
     author='Dan Kelleher',
     author_email='danielkelleher@protonmail.com',
     maintainer='Dan Kelleher',
