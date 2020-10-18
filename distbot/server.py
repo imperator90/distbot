3389c09844e4a35ae4de3a38d731858215bf0465
Dan <danielkelleher@protonmail.com>
2020-10-18 11:45:04 -0400

    new server functions

--- distbot/server.py
+++ distbot/server.py
@@ -1,9 +1,14 @@
 from sanic import Sanic, response
 import pyppeteer.launcher
+from pyppeteer.browser import Browser
 
+from typing import Dict
+from pathlib import Path
 from pprint import pformat
+import asyncio
 import argparse
 import logging
+import re
 
 
 def parse_args():
@@ -16,9 +21,13 @@ def parse_args():
     parser.add_argument('-p',
                         '--port',
                         type=int,
-                        default='80',
+                        default='8000',
                         help='Port to run the server on.')
     return parser.parse_args()
+
+
+# map DevTools endpoint to Browser
+active_browsers = {}
 
 
 app = Sanic("Browser Server")
@@ -31,12 +40,36 @@ async def new_browser(request):
     launch_options = request.json
     logging.info(f"Starting browser: {pformat(launch_options)}")
     browser = await pyppeteer.launcher.launch(launch_options)
+    # save reference to Browser.
+    active_browsers[browser.wsEndpoint] = browser
     # Return the DevTools WebSocket endpoint so a remote client can connect.
     return response.json({
         'dev_tools': browser.wsEndpoint,
         'launch_opts': request.json})
 
 
+@ app.route('/browsers')
+async def browsers(request):
+    browsers_ws = list(active_browsers.keys())
+    logging.info(f"Active browsers: {pformat(browsers_ws)}")
+    return response.json(browsers_ws)
+
+
+@ app.route('/rm_browser')
+async def rm_browser(request):
+    b = request.args['browser'][0]
+    if b in active_browsers:
+        logging.info(f"Shutting down browser: {b}")
+        try:
+            await asyncio.wait_for(active_browsers[b].close(), timeout=5)
+        except asyncio.TimeoutError:
+            logging.warning("Could not propertly close browser.")
+        del active_browsers[b]
+    else:
+        logging.error(f"Unknown browser endpoint: {b}")
+
+
 if __name__ == '__main__':
     args = parse_args()
+
     app.run(host=args.address, port=args.port, debug=True)
