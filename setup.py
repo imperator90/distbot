074647d5b27990441a269376c8334a2d41a6a177
Dan <kelleherjdan@gmail.com>
2020-12-01 16:17:14 -0500

    change version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 setuptools.setup(
     name='distbot',
-    version='2.6.7',
+    version='2.6.8',
     author='Dan Kelleher',
     author_email='danielkelleher@protonmail.com',
     maintainer='Dan Kelleher',
